b491a081dae040ddf9e02070f60d31ea80ccdf10
Jorge Machado <machadoggg@gmail.com>
2023-05-21 03:20:15 -0500

    Complete Customers API controller

--- CustomerManagement.API/Controllers/CustomersController.cs
+++ CustomerManagement.API/Controllers/CustomersController.cs
@@ -24,12 +24,46 @@ namespace CustomerManagement.API.Controllers
             return Ok(await _context.Customers.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        { 
+            var customer = await _context.Customers.SingleOrDefaultAsync(c => c.Codigo == id); 
+            if (customer == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
         [HttpPost]
         public async Task<ActionResult> PostAsync(Customer customer)
         {
             _context.Customers.Add(customer);
             await _context.SaveChangesAsync();
             return Ok(customer);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+            return Ok(customer);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var customer = await _context.Customers.SingleOrDefaultAsync(c => c.Codigo == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
