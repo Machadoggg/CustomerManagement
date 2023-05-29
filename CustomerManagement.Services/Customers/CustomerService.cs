b8976934f7bb82203106157bc9ab735c9d86fa1c
Jorge Machado <machadoggg@gmail.com>
2023-05-28 20:33:59 -0500

    Control exception in service

--- CustomerManagement.Services/Customers/CustomerService.cs
+++ CustomerManagement.Services/Customers/CustomerService.cs
@@ -26,11 +26,30 @@ namespace CustomerManagement.Services.Customers
 
         public async Task<Customer> PostAsync(Customer customer)
         {
-           
+            try
+            {
+                DateTime currentDate = DateTime.Now;
+                int age = currentDate.Year - customer.FechaNacimiento.Year;
+
+                if (age >= 0 && age <= 7 && customer.TipoDocumento != "Registro Civil (RC)")
+                {
+                    throw new Exception("El documento debe ser Registro Civil (RC) para clientes de 0 a 7 años.");
+                }
+                else if (age >= 8 && age <= 17 && customer.TipoDocumento != "Tarjeta Identidad (TI)")
+                {
+                    throw new Exception("El documento debe ser Tarjeta Identidad (TI) para clientes de 8 a 17 años.");
+                }
+                else if (age > 18 && customer.TipoDocumento != "Cedula Ciudadanía (CC)")
+                {
+                    throw new Exception("El documento debe ser Cedula Ciudadanía (CC) para clientes mayores de 18 años.");
+                }
                 var customerPost = await _customerRepository.PostAsync(customer);
                 return customerPost;
-            
-            
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<Customer> PutAsync(Customer customer)
